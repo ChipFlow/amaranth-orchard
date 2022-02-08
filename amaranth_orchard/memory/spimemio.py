942eeff8133dda0b428c5cb371c0a200a4a040a9
gatecat <gatecat@ds0.me>
2022-02-08 13:59:52 +0000

    Don't use CSRs for SPI flash control register
    Signed-off-by: gatecat <gatecat@ds0.me>

--- amaranth_orchard/memory/spimemio.py
+++ amaranth_orchard/memory/spimemio.py
@@ -25,7 +25,7 @@ class SPIMemIO(Peripheral, Elaboratable):
 
     data_bus is a bus peripheral that directly maps the 16MB of read-only flash memory.
 
-    ctrl_bus splits the original 32-bit control register over 4 8-bit CSRs.
+    ctrl_bus is the original 32-bit control register
     """
     def __init__(self, *, flash, **kwargs):
         super().__init__()
@@ -38,10 +38,11 @@ class SPIMemIO(Peripheral, Elaboratable):
         self.data_bus.memory_map = map
 
         bank            = self.csr_bank()
-        self._cfgreg     = [bank.csr(8, "rw", name=f"cfg{i}") for i in range(4)]
-        self._bridge    = self.bridge(data_width=32, granularity=8)
-        self.ctrl_bus = self._bridge.bus
-
+        self.ctrl_bus = wishbone.Interface(addr_width=0,
+                                      data_width=32, granularity=8)
+        map = MemoryMap(addr_width=2, data_width=8)
+        map.add_resource(name="flash_ctrl", size=4, resource=self)
+        self.ctrl_bus.memory_map = map
         self.size = 2**24
 
     def elaborate(self, platform):
@@ -50,6 +51,8 @@ class SPIMemIO(Peripheral, Elaboratable):
         # TODO : QSPI
         oe = Signal(4)
         cfgreg_do = Signal(32)
+        ctrl_enable = Signal()
+
         m.submodules.spimemio = Instance(
             "spimemio",
             i_clk=ClockSignal(),
@@ -72,14 +75,10 @@ class SPIMemIO(Peripheral, Elaboratable):
             i_flash_io1_di=self.flash.d_i[1],
             i_flash_io2_di=self.flash.d_i[2],
             i_flash_io3_di=self.flash.d_i[3],
-            i_cfgreg_we=Cat(self._cfgreg[i].w_stb for i in range(4)),
-            i_cfgreg_di=Cat(self._cfgreg[i].w_data for i in range(4)),
-            o_cfgreg_do=cfgreg_do,
+            i_cfgreg_we=self.ctrl_bus.sel & Repl(ctrl_enable & self.ctrl_bus.we, 4),
+            i_cfgreg_di=self.ctrl_bus.dat_w,
+            o_cfgreg_do=self.ctrl_bus.dat_r,
         )
-        m.submodules.bridge  = self._bridge
-        for i in range(4):
-            m.d.comb += [self._cfgreg[i].r_data.eq(cfgreg_do[i*8:(i+1)*8])]
-
         # From https://github.com/im-tomu/foboot/blob/master/hw/rtl/picorvspi.py
         read_active = Signal()
         with m.If(self.data_bus.stb & self.data_bus.cyc & ~read_active):
@@ -90,6 +89,10 @@ class SPIMemIO(Peripheral, Elaboratable):
             m.d.sync += self.data_bus.ack.eq(1)
         with m.Else():
             m.d.sync += self.data_bus.ack.eq(0)
+
+        prev_ctrl = Signal()
+        m.d.comb += ctrl_enable.eq(self.ctrl_bus.stb & self.ctrl_bus.cyc & ~self.ctrl_bus.ack)
+        m.d.sync += self.ctrl_bus.ack.eq(ctrl_enable)
 
         filename = Path(__file__).parent / f"verilog/spimemio.v"
         with open(filename, 'r') as f:
