9d4aab579befec781c8b67b311bb7d912be0f733
Rob Taylor <rob.taylor@chipflow.io>
2025-03-05 11:48:17 +0000

    Hyperram had bitrotten, move test to pytest and fix up

---
+++ tests/test_hyperram.py
@@ -0,0 +1,37 @@
+from amaranth import Module
+from amaranth.sim import Simulator
+from amaranth_orchard.memory import HyperRAM, HyperRAMPins
+
+def test_hyperram_smoke():
+    pins = HyperRAMPins(cs_count=1)
+    m = Module()
+    m.submodules.hram = hram = HyperRAM(pins=pins)
+    sim = Simulator(m)
+    sim.add_clock(1e-6)
+    def process():
+        yield hram.data_bus.adr.eq(0x5A5A5A)
+        yield hram.data_bus.dat_w.eq(0xF0F0F0F0)
+        yield hram.data_bus.sel.eq(1)
+        yield hram.data_bus.we.eq(1)
+        yield hram.data_bus.stb.eq(1)
+        yield hram.data_bus.cyc.eq(1)
+        for i in range(100):
+            if (yield hram.data_bus.ack):
+                yield hram.data_bus.stb.eq(0)
+                yield hram.data_bus.cyc.eq(0)
+            yield
+        yield hram.data_bus.adr.eq(0x5A5A5A)
+        yield hram.data_bus.sel.eq(1)
+        yield hram.data_bus.we.eq(0)
+        yield hram.data_bus.stb.eq(1)
+        yield hram.data_bus.cyc.eq(1)
+        yield pins.rwds.i.eq(1)
+        yield pins.dq.i.eq(0xFF)
+        for i in range(100):
+            if (yield hram.data_bus.ack):
+                yield hram.data_bus.stb.eq(0)
+                yield hram.data_bus.cyc.eq(0)
+            yield
+    sim.add_sync_process(process)
+    with sim.write_vcd("hyperram.vcd", "hyperram.gtkw"):
+        sim.run()
