9c613bb54b8201fcbc0f94e37562108381dcccc8
Totalsavage <tochichukwukaanyanwu@gmail.com>
2025-07-21 09:31:27 +0100

    gpt with it's specifications for now

--- chipflow_digital_ip/base/gptimer.py
+++ chipflow_digital_ip/base/gptimer.py
@@ -1,1 +1,15 @@
-from 
+from amaranth import *
+from amaranth_soc import csr
+
+class GPTimer(Elaboratable):
+    """
+    General-Purpose Timer (GPTimer) peripheral.
+
+    Features:
+      - 32-bit up-counter
+      - 8-bit prescaler (divide-by N+1)
+      - Single 32-bit compare register
+      - Auto-reload on compare match
+      - Interrupt on match (level-high)
+      - CSR-based register interface
+    """
